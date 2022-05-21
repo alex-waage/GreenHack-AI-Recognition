29043137f1b7bf1b2de096609858eeb438262f14
Vít Svoboda <roboribafish@gmail.com>
2022-05-21 13:19:02 +0200

    add

--- app.py
+++ app.py
@@ -46,71 +46,72 @@ def getInfo():
 
             #################################################
 
-            val_ds = tf.keras.utils.image_dataset_from_directory(
-                data_dir,
-                validation_split=0.2,
-                subset="validation",
-                seed=123,
-                image_size=(img_height, img_width),
-                batch_size=batch_size)
+            # val_ds = tf.keras.utils.image_dataset_from_directory(
+            #     data_dir,
+            #     validation_split=0.2,
+            #     subset="validation",
+            #     seed=123,
+            #     image_size=(img_height, img_width),
+            #     batch_size=batch_size)
 
-            AUTOTUNE = tf.data.AUTOTUNE
+            # AUTOTUNE = tf.data.AUTOTUNE
 
-            train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-            val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+            # train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+            # val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-            normalization_layer = layers.Rescaling(1./255)
+            # normalization_layer = layers.Rescaling(1./255)
 
-            normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-            image_batch, labels_batch = next(iter(normalized_ds))
-            first_image = image_batch[0]
-            # Notice the pixel values are now in `[0,1]`.
-            print(np.min(first_image), np.max(first_image))
+            # normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+            # image_batch, labels_batch = next(iter(normalized_ds))
+            # first_image = image_batch[0]
+            # # Notice the pixel values are now in `[0,1]`.
+            # print(np.min(first_image), np.max(first_image))
 
-            num_classes = len(class_names)
+            # num_classes = len(class_names)
 
-            data_augmentation = keras.Sequential(
-            [
-                layers.RandomFlip("horizontal",
-                                input_shape=(img_height,
-                                            img_width,
-                                            3)),
-                layers.RandomRotation(0.1),
-                layers.RandomZoom(0.1),
-            ]
-            )
+            # data_augmentation = keras.Sequential(
+            # [
+            #     layers.RandomFlip("horizontal",
+            #                     input_shape=(img_height,
+            #                                 img_width,
+            #                                 3)),
+            #     layers.RandomRotation(0.1),
+            #     layers.RandomZoom(0.1),
+            # ]
+            # )
 
-            model = Sequential([
-            data_augmentation,
-            layers.Rescaling(1./255),
-            layers.Conv2D(16, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(32, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Dropout(0.2),
-            layers.Flatten(),
-            layers.Dense(128, activation='relu'),
-            layers.Dense(num_classes)
-            ])
+            # model = Sequential([
+            # data_augmentation,
+            # layers.Rescaling(1./255),
+            # layers.Conv2D(16, 3, padding='same', activation='relu'),
+            # layers.MaxPooling2D(),
+            # layers.Conv2D(32, 3, padding='same', activation='relu'),
+            # layers.MaxPooling2D(),
+            # layers.Conv2D(64, 3, padding='same', activation='relu'),
+            # layers.MaxPooling2D(),
+            # layers.Dropout(0.2),
+            # layers.Flatten(),
+            # layers.Dense(128, activation='relu'),
+            # layers.Dense(num_classes)
+            # ])
 
-            model.compile(optimizer='adam',
-                        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                        metrics=['accuracy'])
+            # model.compile(optimizer='adam',
+            #             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            #             metrics=['accuracy'])
 
-            model.summary()
+            # model.summary()
 
-            epochs = 15
-            history = model.fit(
-            train_ds,
-            validation_data=val_ds,
-            epochs=epochs
-            )
+            # epochs = 15
+            # history = model.fit(
+            # train_ds,
+            # validation_data=val_ds,
+            # epochs=epochs
+            # )
+
+            # model.save("./saved/testModel3")
 
             ##################################
 
-            model.save("./saved/testModel3")
             model = keras.models.load_model('./saved/testModel3')
 
             img = tf.keras.utils.load_img(
